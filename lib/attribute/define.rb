653edb3dfbbc42d6cb075ff7f9a7de0210a0f480
Scott Bellware <sbellware@gmail.com>
2015-02-08 17:28:13 -0600

    'attr_name' rather than 'name'

--- lib/attribute/define.rb
+++ lib/attribute/define.rb
@@ -1,19 +1,19 @@
 module Attribute
   class Define
     attr_reader :target_class
-    attr_reader :name
+    attr_reader :attr_name
     attr_reader :visibility
 
-    def initialize(target_class, name, visibility=nil)
+    def initialize(target_class, attr_name, visibility=nil)
       visibility ||= :reader
 
       @target_class = target_class
-      @name = name
+      @attr_name = attr_name
       @visibility = visibility
     end
 
-    def self.!(target_class, name, visibility)
-      instance = new target_class, name, visibility
+    def self.!(target_class, attr_name, visibility)
+      instance = new target_class, attr_name, visibility
       instance.!
     end
 
@@ -23,16 +23,16 @@ module Attribute
     end
 
     def define_reader
-      name = self.name
-      target_class.send :define_method, name do
-        instance_variable_get("@#{name}")
+      attr_name = self.attr_name
+      target_class.send :define_method, attr_name do
+        instance_variable_get("@#{attr_name}")
       end
     end
 
     def define_writer
-      name = self.name
-      target_class.send :define_method, "#{name}=" do |value|
-        instance_variable_set("@#{name}", value)
+      attr_name = self.attr_name
+      target_class.send :define_method, "#{attr_name}=" do |value|
+        instance_variable_set("@#{attr_name}", value)
       end
     end
   end
