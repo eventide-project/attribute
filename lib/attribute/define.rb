52e0b90c06614740391f72f186fe88864cd3c9cc
Scott Bellware <sbellware@gmail.com>
2015-02-08 17:47:58 -0600

    Using attribute definition code from NullAttr

--- lib/attribute/define.rb
+++ lib/attribute/define.rb
@@ -4,6 +4,8 @@ module Attribute
     attr_reader :attr_name
     attr_reader :visibility
 
+    ## TODO initial value block
+
     def initialize(target_class, attr_name, visibility=nil)
       visibility ||= :reader
 
@@ -23,17 +25,38 @@ module Attribute
     end
 
     def define_reader
-      attr_name = self.attr_name
+      attr_name = :"#{self.attr_name}"
+      var_name = "@#{attr_name}"
       target_class.send :define_method, attr_name do
-        instance_variable_get("@#{attr_name}")
+        val = instance_variable_get(var_name)
+
+        # unless val
+        #   val = NullObject.build interface
+        #   instance_variable_set var_name, val
+        # end
+
+        val
       end
+
+      # attr_name = self.attr_name
+      # target_class.send :define_method, attr_name do
+      #   instance_variable_get("@#{attr_name}")
+      # end
     end
 
     def define_writer
-      attr_name = self.attr_name
-      target_class.send :define_method, "#{attr_name}=" do |value|
-        instance_variable_set("@#{attr_name}", value)
+      # attr_name = self.attr_name
+      # target_class.send :define_method, "#{attr_name}=" do |value|
+      #   instance_variable_set("@#{attr_name}", value)
+      # end
+
+      attr_name = :"#{self.attr_name}"
+      writer_name = :"#{attr_name}="
+      var_name = "@#{attr_name}"
+      target_class.send :define_method, writer_name do |val|
+        instance_variable_set var_name, val
       end
+
     end
   end
 end
