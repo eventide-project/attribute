b418d81a9e1736ebf49791fe6dbb483891c56a89
Scott Bellware <sbellware@gmail.com>
2015-11-10 18:24:43 -0600

    Symlink scripts are specialized for this library

--- remove-lib-symlinks.sh
+++ remove-lib-symlinks.sh
@@ -1,3 +1,3 @@
 source ./library-symlinks.sh
 
-remove_lib_symlinks 'subst_attr'
+remove_lib_symlinks 'attribute'
--- symlink-lib.sh
+++ symlink-lib.sh
@@ -1,3 +1,3 @@
 source ./library-symlinks.sh
 
-symlink_lib 'subst_attr'
+symlink_lib 'attribute'
