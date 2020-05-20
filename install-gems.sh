c01ebe003c964c214eec8fca7a0b36b9ec91c08b
Nathan Ladd <nathanladd@gmail.com>
2020-05-20 10:51:30 -0500

    Bundler is removed from install-gems.sh

--- install-gems.sh
+++ install-gems.sh
@@ -13,14 +13,32 @@ echo
 echo "Installing gems locally (posture: $posture)"
 echo '= = ='
 
-cmd="bundle install --standalone --path=./gems"
+rm -rf Gemfile.lock ./gems
 
-if [ operational == "$posture" ]; then
-  cmd="$cmd --without=development"
+cmd="gem install --install-dir=./gems --bindir=./gems/bin --file Gemfile --lock"
+
+if [ operational != "$posture" ]; then
+  cmd="$cmd --development"
 fi
 
 echo $cmd
 ($cmd)
+
+echo
+echo "Writing standalone bundle loader file"
+echo "- - -"
+echo
+
+mkdir -p ./gems/bundler
+
+for gem in $(ls ./gems/gems); do
+  load_path="$:.unshift(File.expand_path('../gems/$gem/lib', __dir__))"
+
+  echo $load_path >> ./gems/bundler/setup.rb
+done
+
+cat ./gems/bundler/setup.rb
+echo
 
 echo '- - -'
 echo '(done)'
